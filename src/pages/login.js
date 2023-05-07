5820c1efb01a6b4c51ccb66104d6276195c98776
rajnandini1900 <tanmayc2001@gmail.com>
2023-05-07 13:36:59 +0530

    Commited

--- src/pages/login.js
+++ src/pages/login.js
@@ -16,7 +16,7 @@ const LoginSignupPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://packing-movers.vercel.app/login', {
+      const response = await axios.post('http://localhost:3001/api/login', {
         username,
         password,
       });
